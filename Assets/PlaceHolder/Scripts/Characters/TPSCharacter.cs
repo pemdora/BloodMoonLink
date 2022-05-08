a002f8bd07d7804bad8bf91ec50c9a2de6df83c2
tarazhong <zhong.tara@gmail.com>
2022-05-08 14:27:35 +0200

    Added : Raw character with blend tree

---
+++ Assets/PlaceHolder/Scripts/Characters/TPSCharacter.cs
@@ -0,0 +1,235 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))] // Need a rigidbody
+[RequireComponent(typeof(CapsuleCollider))] // Need any kind of collider
+[RequireComponent(typeof(Animator))]
+public class TPSCharacter : MonoBehaviour
+{
+    [SerializeField]
+    private float m_MovingTurnSpeed = 360;
+    [SerializeField]
+    private float m_StationaryTurnSpeed = 180;
+    [SerializeField]
+    private float m_JumpPower = 12f;
+    [Range(1f, 4f)] [SerializeField]
+    private float m_GravityMultiplier = 2f;
+    [SerializeField]
+    private float m_RunCycleLegOffset = 0.2f; //specific to the character in sample assets, will need to be modified to work with others
+    [SerializeField]
+    private float m_MoveSpeedMultiplier = 1f;
+    [SerializeField]
+    private float m_AnimSpeedMultiplier = 1f;
+    [SerializeField]
+    private float m_GroundCheckDistance = 0.1f;
+
+    private Rigidbody rigidbodyMainCharact;
+    private Animator animator;
+    private bool isGrounded;
+    private float origGroundCheckDistance;
+    private const float k_Half = 0.5f;
+    private float turnAmount;
+    private float forwardAmount;
+    private Vector3 groundNormal;
+    private float capsuleHeight;
+    private Vector3 capsuleCenter;
+    private CapsuleCollider capsule;
+    private bool isCrouching;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbodyMainCharact = GetComponent<Rigidbody>();
+        capsule = GetComponent<CapsuleCollider>();
+        capsuleHeight = capsule.height;
+        capsuleCenter = capsule.center;
+
+        rigidbodyMainCharact.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        origGroundCheckDistance = m_GroundCheckDistance;
+    }
+
+    public void Move(Vector3 move, bool crouch, bool jump)
+    {
+        // convert the world relative moveInput vector into a local-relative
+        // turn amount and forward amount required to head in the desired
+        // direction.
+        if (move.magnitude > 1f) move.Normalize();
+        move = transform.InverseTransformDirection(move);
+        CheckGroundStatus();
+        move = Vector3.ProjectOnPlane(move, groundNormal);
+        turnAmount = Mathf.Atan2(move.x, move.z);
+        forwardAmount = move.z;
+
+        ApplyExtraTurnRotation();
+
+        // control and velocity handling is different when grounded and airborne:
+        /*
+        if (isGrounded)
+        {
+            HandleGroundedMovement(crouch, jump);
+        }
+        else
+        {
+            HandleAirborneMovement();
+        }*/
+
+        //ScaleCapsuleForCrouching(crouch);
+        //PreventStandingInLowHeadroom();
+
+        // send input and other state parameters to the animator
+        UpdateAnimator(move);
+    }
+    
+    void UpdateAnimator(Vector3 move)
+    {
+        // update the animator parameters
+        animator.SetFloat("Forward", forwardAmount, 0.1f, Time.deltaTime);
+        animator.SetFloat("Turn", turnAmount, 0.1f, Time.deltaTime);
+        //animator.SetBool("Crouch", isCrouching);
+        animator.SetBool("OnGround", isGrounded);
+
+        /*
+        if (!isGrounded)
+        {
+            animator.SetFloat("Jump", rigidbodyMainCharact.velocity.y);
+        }*/
+
+        // calculate which leg is behind, so as to leave that leg trailing in the jump animation
+        // (This code is reliant on the specific run cycle offset in our animations,
+        // and assumes one leg passes the other at the normalized clip times of 0.0 and 0.5)
+        float runCycle = Mathf.Repeat(animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
+        //float jumpLeg = (runCycle < k_Half ? 1 : -1) * forwardAmount;
+        
+        /*
+        if (isGrounded)
+        {
+            animator.SetFloat("JumpLeg", jumpLeg);
+        }*/
+
+        // the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
+        // which affects the movement speed because of the root motion.
+
+        if (isGrounded && move.magnitude > 0)
+        {
+            animator.speed = m_AnimSpeedMultiplier;
+        }
+        else
+        {
+            // don't use that while airborne
+            animator.speed = 1;
+        }
+    }
+
+    #region Crouching
+    /*
+    void ScaleCapsuleForCrouching(bool crouch)
+    {
+        if (m_IsGrounded && crouch)
+        {
+            if (m_Crouching) return;
+            m_Capsule.height = m_Capsule.height / 2f;
+            m_Capsule.center = m_Capsule.center / 2f;
+            m_Crouching = true;
+        }
+        else
+        {
+            Ray crouchRay = new Ray(m_Rigidbody.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
+            float crouchRayLength = m_CapsuleHeight - m_Capsule.radius * k_Half;
+            if (Physics.SphereCast(crouchRay, m_Capsule.radius * k_Half, crouchRayLength, Physics.AllLayers, QueryTriggerInteraction.Ignore))
+            {
+                m_Crouching = true;
+                return;
+            }
+            m_Capsule.height = m_CapsuleHeight;
+            m_Capsule.center = m_CapsuleCenter;
+            m_Crouching = false;
+        }
+    }
+    void PreventStandingInLowHeadroom()
+    {
+        // prevent standing up in crouch-only zones
+        if (!m_Crouching)
+        {
+            Ray crouchRay = new Ray(m_Rigidbody.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
+            float crouchRayLength = m_CapsuleHeight - m_Capsule.radius * k_Half;
+            if (Physics.SphereCast(crouchRay, m_Capsule.radius * k_Half, crouchRayLength, Physics.AllLayers, QueryTriggerInteraction.Ignore))
+            {
+                m_Crouching = true;
+            }
+        }
+    }*/
+    #endregion
+
+    /*
+    void HandleAirborneMovement()
+    {
+        // apply extra gravity from multiplier:
+        Vector3 extraGravityForce = (Physics.gravity * m_GravityMultiplier) - Physics.gravity;
+        rigidbodyMainCharact.AddForce(extraGravityForce);
+
+        m_GroundCheckDistance = rigidbodyMainCharact.velocity.y < 0 ? origGroundCheckDistance : 0.01f;
+    }
+
+
+    void HandleGroundedMovement(bool crouch, bool jump)
+    {
+        // check whether conditions are right to allow a jump:
+        if (jump && !crouch && animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded"))
+        {
+            // jump!
+            rigidbodyMainCharact.velocity = new Vector3(rigidbodyMainCharact.velocity.x, m_JumpPower, rigidbodyMainCharact.velocity.z);
+            isGrounded = false;
+            animator.applyRootMotion = false;
+            m_GroundCheckDistance = 0.1f;
+        }
+    }
+    */
+
+    void ApplyExtraTurnRotation()
+    {
+        // help the character turn faster (this is in addition to root rotation in the animation)
+        float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, forwardAmount);
+        transform.Rotate(0, turnAmount * turnSpeed * Time.deltaTime, 0);
+    }
+
+
+    public void OnAnimatorMove()
+    {
+        // we implement this function to override the default root motion.
+        // this allows us to modify the positional speed before it's applied.
+        if (isGrounded && Time.deltaTime > 0)
+        {
+            Vector3 v = (animator.deltaPosition * m_MoveSpeedMultiplier) / Time.deltaTime;
+
+            // we preserve the existing y part of the current velocity.
+            v.y = rigidbodyMainCharact.velocity.y;
+            rigidbodyMainCharact.velocity = v;
+        }
+    }
+
+
+    void CheckGroundStatus()
+    {
+        RaycastHit hitInfo;
+#if UNITY_EDITOR
+        // helper to visualise the ground check ray in the scene view
+        Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * m_GroundCheckDistance));
+#endif
+        // 0.1f is a small offset to start the ray from inside the character
+        // it is also good to note that the transform position in the sample assets is at the base of the character
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance))
+        {
+            groundNormal = hitInfo.normal;
+            isGrounded = true;
+            animator.applyRootMotion = true;
+        }
+        else
+        {
+            isGrounded = false;
+            groundNormal = Vector3.up;
+            animator.applyRootMotion = false;
+        }
+    }
+}
---
+++ Assets/PlaceHolder/Scripts/Characters/TPSUserController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+/// <summary>
+/// TPSController is responsible for main user Input
+/// 
+/// </summary>
+[RequireComponent(typeof(TPSCharacter))]
+public class TPSUserController : MonoBehaviour
+{
+    private TPSCharacter mainCharacter; // A reference to the ThirdPersonCharacter on the object
+    // A reference to the main camera in the scenes transform
+    // We use main camera reference to adapt player movement to it (player will move foward camera looking direction)
+    private Transform mainCamera;
+    private Vector3 mainCameraFoward; // The current forward direction of the camera
+    private Vector3 mainCharacterMove;  // the world-relative desired move direction, calculated from the camForward and user input.
+    private bool isJumping;
+
+    private void Start()
+    {
+        // get the transform of the main camera
+        if (Camera.main != null)
+        {
+            mainCamera = Camera.main.transform;
+        }
+        else
+        {
+            Debug.LogWarning(
+                "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
+            // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
+        }
+
+        // get the third person character ( this should never be null due to require component )
+        mainCharacter = GetComponent<TPSCharacter>();
+    }
+
+    /*
+    private void Update()
+    {
+        if (!isJumping)
+        {
+            isJumping = CrossPlatformInputManager.GetButtonDown("Jump");
+        }
+    }*/
+
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        // read inputs
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+        //bool crouch = Input.GetKey(KeyCode.C);
+
+        // calculate move direction to pass to character
+        if (mainCamera != null)
+        {
+            // calculate camera relative direction to move:
+            mainCameraFoward = Vector3.Scale(mainCamera.forward, new Vector3(1, 0, 1)).normalized;
+            mainCharacterMove = v * mainCameraFoward + h * mainCamera.right;
+        }
+        else
+        {
+            // we use world-relative directions in the case of no main camera
+            mainCharacterMove = v * Vector3.forward + h * Vector3.right;
+        }
+#if !MOBILE_INPUT
+        // walk speed multiplier
+        if (Input.GetKey(KeyCode.LeftShift)) mainCharacterMove *= 0.5f;
+#endif
+
+        // pass all parameters to the character control script
+        mainCharacter.Move(mainCharacterMove,false, isJumping); 
+        isJumping = false;
+    }
+}
